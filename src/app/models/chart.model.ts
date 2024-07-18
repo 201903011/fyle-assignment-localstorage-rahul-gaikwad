8fa71a0d6d119b90de3adca4f8f276261e614f09
201903011 <rahulkishorgaikwad@gmail.com>
2024-07-18 15:33:47 +0530

    chart integration

---
+++ src/app/models/chart.model.ts
@@ -0,0 +1,12 @@
+export class Chart {
+    x: string;
+    y: string;
+
+    constructor(x: string, y: string) {
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+}
--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -1,12 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
+import * as ApexCharts from 'apexcharts';
 import { Subject, takeUntil } from 'rxjs';
 import { User, WorkOut } from 'src/app/models';
+import { Chart } from 'src/app/models/chart.model';
 import { LocalstorageService } from 'src/app/services';
+
 
 @Component({
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule,],
     templateUrl: './profile.component.html',
 })
 export class ProfileComponent {
@@ -18,6 +21,7 @@ export class ProfileComponent {
 
     user: User | null = null;
 
+
     ngOnInit(): void {
 
         this._localStorageService.userDataList$
@@ -27,6 +31,8 @@ export class ProfileComponent {
                     this.user = element;
                 }
             })));
+        const chart = new ApexCharts(document.getElementById('column-chart'), this.getOptions(this.user?.workouts ?? []));
+        chart.render();
     }
 
     ngOnDestroy(): void {
@@ -50,5 +56,95 @@ export class ProfileComponent {
             return "";
         }
     }
+
+    getOptions(workOuts: WorkOut[]): any {
+        const ret = workOuts.map(e => new Chart(e.type, e.minutes.toString()));
+
+        const options = {
+            colors: ["#1A56DB"],
+            series: [
+                {
+                    name: "WorkOut",
+                    color: "#1A56DB",
+                    data: ret,
+                },
+            ],
+            chart: {
+                type: "bar",
+                height: "320px",
+                fontFamily: "Inter, sans-serif",
+                toolbar: {
+                    show: false,
+                },
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    columnWidth: "70%",
+                    borderRadiusApplication: "end",
+                    borderRadius: 8,
+                },
+            },
+            tooltip: {
+                shared: true,
+                intersect: false,
+                style: {
+                    fontFamily: "Inter, sans-serif",
+                },
+            },
+            states: {
+                hover: {
+                    filter: {
+                        type: "darken",
+                        value: 1,
+                    },
+                },
+            },
+            stroke: {
+                show: true,
+                width: 0,
+                colors: ["transparent"],
+            },
+            grid: {
+                show: false,
+                strokeDashArray: 4,
+                padding: {
+                    left: 2,
+                    right: 2,
+                    top: -14
+                },
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            legend: {
+                show: false,
+            },
+            xaxis: {
+                floating: false,
+                labels: {
+                    show: true,
+                    style: {
+                        fontFamily: "Inter, sans-serif",
+                        cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+                    }
+                },
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks: {
+                    show: false,
+                },
+            },
+            yaxis: {
+                show: false,
+            },
+            fill: {
+                opacity: 1,
+            },
+        }
+        return options;
+    }
+
 
 }
